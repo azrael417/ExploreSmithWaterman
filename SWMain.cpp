92088f1e9f44320d8272512734dde28bf3607f08
Brandon <bgcook@lbl.gov>
2018-07-26 12:35:17 -0700

    print alignments per second

--- SWMain.cpp
+++ SWMain.cpp
@@ -141,7 +141,7 @@ int main(int argc, char* argv[]) {
   float cpu_time = (static_cast<float>(end - start)) / static_cast<float>(CLOCKS_PER_SEC);
   fprintf(stdout, "CPU time: %f seconds\n", cpu_time);
   fprintf(stdout, "Number of total alignments: %i\n", num_total_aligns);
-
+  fprintf(stdout, "Alignments per second: %f\n", (float)num_total_aligns / cpu_time);
 	//=====================================================
 	// defind the hash region
 	// first.first:   reference begin
